65214dae4e9071ac50a7edae7b3a9596143fea97
Deepak-Valaiyabathi <v.deepak8020@gmail.com>
2024-09-02 14:30:10 +0530

    Initial Commit - Portfolio website

---
+++ index.js
@@ -0,0 +1,88 @@
+
+// // a=100
+// // b=6
+// // a=a+b
+// // b=a-b
+// // a=a-b
+// // console.log(a,b); 
+
+// // a=100;
+// // b=7;
+// // console.log(a,b);
+// // a=a^b;
+// // b=a^b;
+// // a=a^b;
+// // console.log(a,b);
+
+// // x=999999999;
+// // x=x^x^x^x^x^x^x^x^x;
+// // console.log(x);
+
+// // p=12;
+// // q=5;
+// // let r = p^q;
+// // let s = ~p;
+// // console.log(r);
+// // console.log(s);
+
+// // let m = 7;   // 0111 in binary
+// // let n = 8;   // 1000 in binary
+// // let o = (m + n) >> 1;
+// // let t = o | n;
+// // console.log(o);
+// // console.log(t);
+
+// let a = 15;  // 1111 in binary
+// let b = 2;
+// let c = a << b;
+// let d = c + b;
+// let e = d & a;
+// console.log(a)
+// console.log(d)
+// console.log(e)
+
+// let name="Deepak";
+// let schoolName="Bishop"
+// let englishMark=47;
+// let mathsMark=47;
+// // console.log("My name: "+name);
+// // console.log("School name: "+schoolName);
+// // console.log();
+// if(englishMark==mathsMark){
+//     console.log("english and math mark is same")
+// }else{
+//     console.log("maths and english mark is not same"); 
+// }
+
+// let num= 0;
+// if (num>0){
+//     console.log("The number is positive")
+// }else if (num==0){
+//      console.log("The number is zero")   
+// }else{
+//     console.log("The number is negative") 
+// }
+
+// let vowel="r";
+// if (vowel=="a"){
+//     console.log("The character is a vowel")
+// }else if (vowel=="e"){
+//     console.log("The character is a vowel") 
+// }
+// else if (vowel=="i"){
+//     console.log("The character is a vowel") 
+// }
+// else if (vowel=="o"){
+//     console.log("The character is a vowel") 
+// }
+// else if (vowel=="u"){
+//     console.log("The character is a vowel") 
+// }
+// else{
+//     console.log("The character is a consonant")   
+// }
+// if(vowel=="a"||vowel=="e"||vowel=="i"||vowel=="o"||vowel=="u"){
+//     console.log("The character is a vowel")
+// }else{
+//         console.log("The character is a consonant")  
+// }
