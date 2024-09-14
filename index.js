c26e7248a5b6b5a8ceb900702993d91a58f588b4
Deepak-Valaiyabathi <v.deepak8020@gmail.com>
2024-09-14 12:40:47 +0530

    Change the colors and use @media querie

--- index.js
+++ index.js
@@ -86,10 +86,464 @@
 // }else{
 //         console.log("The character is a consonant")  
 // }
-let memAmount=470;
-let points=memAmount/10;
-if (memAmount>10){
-    console.log("Your member point:"+points)
-}else{
-    console.log("not eligible for member");  
+// let memAmount=470;
+// let points=memAmount/10;
+// if (memAmount>10){
+//     console.log("Your member point:"+points)
+// }else{
+//     console.log("not eligible for member");  
+// }
+// let cc = 26;
+// if (cc){
+//     console.log("hi")
+// }else{
+//     console.log("ih")
+// }
+// if (cc == 26){
+//     console.log("hi")
+// }else{
+//     console.log("ih")
+// }
+// let p = 20000;
+// let n = 2;
+// let r = 6;
+// let SI = p*n*r/100;
+// let per = SI/p*100;
+// if (per>=50){
+//     console.log("percentage 50% above",per)
+// }else{
+//     console.log("percentage 50% below",per);   
+// }
+// let t = 63;
+// let e = 49;
+// let m = 35;
+// let s = 55;
+// let ss = 72;
+// let tot = t + e + m + s + ss;
+// let percentage = tot/5;
+// if (percentage>=50){
+//     console.log("percentage 50% above",percentage)
+// }else{
+//     console.log("percentage 50% below",percentage);   
+// }
+// let count=1;
+// while (count<10){
+//     count++;
+//     console.log("value of counts: "+count); 
+// }
+// let roll=1001;
+// while (roll<1027){
+//     console.log("R.No: "+roll);
+//     roll++;
+// }
+// printable(2,11, 1)
+// printable(3,111, 101)
+// printable(4,1011, 1001)
+// printable(5,11, 1)
+// printable(6,11, 1)
+// printable(7,11, 1)
+// printable(8,11, 1)
+// printable(9,11, 1)
+// printable(10,11, 1)
+// printable(11,11, 1)
+// function printable(tableValue, counter, tab) {
+//     while (tab<counter){
+//         console.log(tab,"*",tableValue,"=",tab*tableValue);
+//         tab++;  
+//     }  
+// }
+// let i = 100;
+// do{
+//     console.log("Value",i);
+//     i++
+// }while(i<11);
+
+// let j = 2;
+// while(j<11){
+//     console.log("value", j);
+//     j++
+// }
+// do{
+//     console.log("true");
+//     i++
+// }while(i<11);
+// while(i>=1){
+//     console.log("value: ",i);
+//     i--
+// }
+// let output = "";
+// let num = 100;
+
+// while (num >= 1) {
+//     output += num + " ";  // Append the number and a space to the output string
+//     num--;
+// }
+// console.log(output.trim()); 
+// console.log("Try programiz.pro");
+// console.log("Value of added numbers ===>"+ fresh(10,20));
+// function fresh(a, b) {
+//     return a+b;
+// }
+
+// console.log(add(33,27));
+// console.log(sub(33,27));
+// console.log(mul(33,27));
+// console.log(div(33,27));
+// //add
+// function add(c,d) {
+//     return c+d;
+// }
+// // sub
+// function sub(c,d) {
+//     return c-d;
+// }
+// // mul
+// function mul(c,d) {
+//     return c*d;
+// }
+// function div(c,d) {
+//     return c/d;
+// }
+// console.log(student_name("Deepak"));
+// console.log(roll_number("R.No: ",20508798));
+// console.log(academy("FSSA"));
+// function student_name(name) {
+//     return name;
+// }
+// function roll_number(roll,number) {
+//     return roll+number;
+// }
+// function academy(a_name) {
+//     return a_name;
+// }
+// var value = 10;
+// while(value>=1){
+//     console.log(squre(value));
+//     value--
+// }
+// function squre(value) {
+//     return value*value;
+// }
+// var value = 10;
+// while(value>=1){
+//     console.log(qube(value));
+//     value--
+// }
+// function qube(value) {
+//     return value*value*value;
+// }
+// kabali(2,21)
+// kabali(3,21)
+// kabali(4,21)
+// kabali(5,21)
+// kabali(6,21)
+// kabali(7,21)
+// kabali(8,21)
+// kabali(9,21)
+// kabali(10,21)
+// function kabali(table, count) {
+//     let i = 1;
+//     while (i<count) {
+//         console.log(i+" * "+table+" = "+i*table);
+//         i++
+//     }
+// }
+// returnn(10)
+// function returnn(n){
+//    while (n>=1) {
+//     console.log(n*n);
+//     n--
+//    }
+// }
+// num(5)
+// function num(n) {
+//     let i = 1;
+//     let output = " ";
+//      while (n >= i) {
+//         output +=  "+" + n; 
+//         n--;
+//     }
+//     console.log(output.trim());   
+// }
+// console.log("Even Numbers: "+EvenNumbers(1,2));
+// function EvenNumbers(num, even) {
+//     let i = 11;
+//     while (num<i) {
+//         num=num+even;
+//         num++
+//     }
+//     return num;
+// }
+// console.log(sumNumbers(5))
+// function sumNumbers(n) {
+//     while (1 < n) {
+//       n++;
+//     }
+//     return n;
+//   }
+// printEvenNumbers(10)
+// function printEvenNumbers(n) {
+//     let i = 1;
+//     while (i <= n) {
+        
+//         if (i % 2 === 0) {
+//             console.log(i);  
+//         }
+//         i++;
+//     }
+// }
+// console.log(sumNumbers(10));
+// function sumNumbers(n) {
+//     let sum = 0;
+//     let i = 1;
+//     while (i <=n) {
+//         sum +=i;
+//       i++;
+//     }
+//     return sum;
+//   }
+// sumnumber(3)
+// function sumnumber(n) {
+//     let sum = 0;
+//     let i = 1;
+//     while (i<=n) {
+//         sum += i;
+//         i++
+//     }
+//     console.log(sum);  
+// }
+// console.log("Facter Value: ",facter(6))
+// function facter(m) {
+//     let j = 1;
+//     let summ = 1;
+//     while (j<=m) {
+//         summ = summ * j;
+//         j++;
+//     }
+//     return summ;
+// }
+// for loop syntax:
+//  for(x=1; x<=5; x++){
+//   console.log(x);
+// }
+// for(x=5; x>=1; x--){
+//     console.log(x);
+//   }
+// let sum=1;
+//   for(x=1; x<=5; x++){  
+//     sum=sum*x;
+//   }
+//   console.log(sum);
+// console.log(add(10));
+
+// function add(x) {
+//     x=x+6;
+//     return "hi " + x;
+// }
+// function finder(j,k) {
+//     while (j<=k) {
+
+//         findodd(j)
+//         function findodd(j) {
+//                 if (j % 2!=0) {
+//                     console.log("Odd Number");
+//                 }
+//                 else{
+//                     console.log("Even Number");
+                    
+//                 }
+//         }
+//         findeven(j)
+//         function findeven(j) {
+//                 if (j % 2==0) {
+//                     console.log("Even Number") 
+//                 }
+//         }  
+//     }
+// }
+
+
+
+// range(1, 100, 9)
+// function range(irange, frange, divi) {
+//     let output = " ";
+//     while (irange<=frange) {
+//         if (irange % divi === 0) { //intial range divide to divi prarameter.
+//             output += irange + " "; // empty output variable connect to irange.
+//         }
+//         irange++;
+//     }
+//     console.log("end result: ",output);
+// }
+
+// console.log(EvenOrOdd(5))
+// function EvenOrOdd(n){
+//     if (n % 2 == 0){
+//         return "Even"
+//     }else {
+//         return "Odd"
+//     }
+// }
+// while(i<100)
+function rocket(s,e){
+    while(s>=e){
+        if(s==3){
+            console.log("get");
+        }else if (s==2){
+            console.log("set");
+        }else if (s==1){
+            console.log("Rocket launch");
+        }else{
+            console.log(s);
+        }
+        s--
+    }
 }
+// const readline = require('readline');
+// // Create interface to get input from the user
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+// // Ask the user for the start range
+// rl.question('Enter start range: ', (userInputStartRange) => {
+//     // Ask the user for the end range
+//     rl.question('Enter end range: ', (userInputEndRange) => {
+//             // Ask the user to have a skip interval
+//         rl.question('Enter skip interval : ', (userSkipInterval) => {
+//         let startNumber = parseInt(userInputStartRange);
+//         let endNumber = parseInt(userInputEndRange);
+//         let skipInterval = parseInt(userSkipInterval);
+//             if (skipInterval === 0) {
+//                 console.log("Skip interval cannot be zero.");
+//                 rl.close();
+//                 return;
+//             }
+//         print("*** Printing numbers between "
+//          + startNumber + " and " + endNumber + " ***");
+//         if(startNumber < endNumber){
+//             printRangeAscending(startNumber, endNumber, skipInterval);
+//         }else{
+//             printRangeDescending(startNumber, endNumber, skipInterval);
+//         }
+//         rl.close();
+//     });
+// });
+// });
+//   function printRangeAscending(startNumber, endNumber, skipInterval){
+//       while(startNumber < endNumber){
+//           console.log(startNumber);
+//           startNumber = startNumber+skipInterval;
+//       }
+//   }
+//   function printRangeDescending(startNumber, endNumber, skipInterval){
+//     while(startNumber > endNumber){
+//         console.log(startNumber);
+//         startNumber = startNumber-skipInterval;
+//     }
+// }
+// function print(message){
+// console.log(message);
+// }
+
+let firstnumber = 100;
+let lastnumber = 10;
+let skip = 4;
+
+if (firstnumber<lastnumber){
+    printRangeAscending(firstnumber, lastnumber, skip)
+}else{
+    printRangeDescending(firstnumber, lastnumber, skip)
+}
+
+
+function printRangeAscending(firstnumber, lastnumber, skip){
+    while(firstnumber<lastnumber){
+        console.log(firstnumber);
+        firstnumber=firstnumber+skip;
+    }
+}
+
+function printRangeDescending(firstnumber, lastnumber, skip){
+    while(firstnumber>lastnumber){
+        console.log(firstnumber);
+        firstnumber=firstnumber-skip
+    }
+}
+
+sathyacinemas(7)
+function sathyacinemas(ticket){
+   let overallticket = 1000;
+   let soldoutticket = 993;
+   let free = overallticket-soldoutticket;
+    if (ticket==free){
+        console.log("Thank you for watching");
+    }else if (ticket<=free){
+        console.log(ticket-free+": only ticket available");
+    }else{
+        console.log("only "+free+" tickets available");
+    }
+}
+
+classifyNumber(15)
+function classifyNumber(n){
+    if(n%3==0 & n%5==0){
+        console.log(n+" Divisible by both 3 and 5");
+    }else if(n%3==0){
+        console.log(n+" Divisible by 3");
+    }else if(n%5==0){
+        console.logn+(n+" Divisible by 5");
+    } else{
+        console.log(n+" Not divisible by 3 or 5")
+    }
+  }
+  getDayName(6)
+  function getDayName(d){
+  switch (d) {
+      case 1:
+        console.log("Sunday");
+        break;
+      case 2:
+          console.log("Monday");
+          break;
+      case 3:
+          console.log("Tuesday");
+          break;
+      case 4:
+          console.log("Wednesday");
+          break;
+      case 5:
+          console.log("Thursday");
+          break;
+      case 6:
+          console.log("Friday");
+          break;
+      case 7:
+          console.log("Saturday");
+          break;
+      default:
+          console.log("Invalid day");
+          break;
+  }
+
+  }
+//   m =5 and r = 6
+printMultiplicationTable(5,6) 
+  function printMultiplicationTable(m, r){
+    let i = 0;
+    while (i<r) {
+        console.log(r+" x "+m+" = "+m*r);
+        r--
+    }
+  }
+bank(1000, 50)
+  function bank(withdrawamount, noofwithdraw) {
+    let total=50000;
+    if (withdrawamount<=total) {
+        total=total-withdrawamount*noofwithdraw
+        console.log(total+ " Available balance");
+    }else{
+        console.log("Available balance "+total);
+        
+    }
+  }
